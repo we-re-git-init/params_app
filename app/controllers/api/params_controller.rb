19d4a4a691c9d83397cdfe83de0c26cbe71a7f25
Brian <riceb53@gmail.com>
2019-06-26 19:31:45 -0500

    first letter of A

--- app/controllers/api/params_controller.rb
+++ app/controllers/api/params_controller.rb
@@ -1,7 +1,13 @@
 class Api::ParamsController < ApplicationController
   def query
-    p 'params hahahaha'
-    @name = params["name"].reverse
+    name = params["name"]
+    first_character = params["name"][0]
+    if first_character == 'a'
+      @message = "The first letter of your name is A"
+    else
+      @message = "Your name is #{name}"
+    end
+
     render 'query_parameter.json.jb'
   end
 end
